8a0199e9993e3741169811a3be6e6dc1a3b4aac2
Dev <rifanwot@gmail.com>
2023-12-19 23:22:55 +0200

    update

---
+++ src/main/java/com/lesson/RpgGame.java
@@ -0,0 +1,66 @@
+package com.lesson;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RpgGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("Введите имя героя ");
+
+        String heroName = scanner.next();
+        int heroHP = 15;
+        int heroAttack = 5;
+
+        String monsterName = "Орка";
+        int monsterHP = 15;
+        int monsterAttack = 3;
+
+        System.out.println("Старт игры");
+        System.out.println("Герой " + heroName + " вошел в лес и увидел " + monsterName + ". " +
+                "Игра началалсь");
+        while (true) {
+            System.out.println("Режим битвы");
+            System.out.println("Выбери действие: 1.Атака 2.Защита 3.Уклонение                                                                                                                               ");
+            int command = scanner.nextInt();
+            if (command == 1) {
+                System.out.println("Герой  атакует " + monsterName + " наносит " + monsterHP + " урона");
+                monsterHP -= heroAttack;
+                if(random.nextInt(100)< 10){
+                    System.out.println("Герой смог нанести дополнительный урон " +heroAttack+ " ед. урона");
+                    monsterHP-=heroAttack;
+                }
+                System.out.println("У монстра " + monsterName + " осталось " + monsterHP + "здоровья");
+            } else if (command == 2) {
+                System.out.println("Герой " + heroName + " использовал защиту и восполнил HP ");
+                heroHP++;
+            } else {
+                System.out.println("Пропуск хода ");
+            }
+
+            System.out.println("У героя " + heroName + " сейчас " + heroHP + " единиц здоровья ");
+
+            System.out.println("Ход монстра ");
+            int monsterCommand = random.nextInt(1);
+            if(monsterCommand == 1) {
+                System.out.println("Монстр  Атакует  " + heroName + " наносит " + monsterAttack + " Урона");
+                heroHP -= monsterAttack;
+                if (heroHP <= 0) {
+                    System.out.println("Монстр победил");
+                    break;
+                }
+            }else if (monsterCommand == 1){
+                System.out.println("Монстр  отдыхает");
+                monsterHP+=2;
+
+            }if(monsterHP<=0){
+                System.out.println("Монстр проиграл");
+            }
+            System.out.println("Герой " + heroName + " имеет " + heroHP + " единиц здоровья ");
+
+        }
+
+         System.out.println("Игра окончена");
+    }
+}
