7a4399f406b3eb4cdb8b97a33d35921ca4693748
Dev <rifanwot@gmail.com>
2024-01-21 17:42:00 +0200

    FixShop

--- src/main/java/com/lesson/Game.java
+++ src/main/java/com/lesson/Game.java
@@ -11,7 +11,7 @@ class Game {
     private Scanner scanner;
     private Random random;
     Shop shop;
-    private int dropGold;
+    
 
     public Game(String heroName) {
         scanner = new Scanner(System.in);
@@ -83,7 +83,8 @@ class Game {
         if (hero.isAlive()) {
 
             System.out.println("Герой " + hero.getName() + " победил!");
-            System.out.println("Получено " + dropGold + " золота.");
+            int droppedGold = monster.droppedGold();
+            System.out.println("Получено " + droppedGold + " золота.");
         } else {
             System.out.println("Монстр " + monster.getName() + " победил!");
         }
